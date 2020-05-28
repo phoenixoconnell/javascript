73fbf3f72b89b8a83b638111c53bdd4268e055f4
Phoenix Shane <phoenix.oconnell@gmail.com>
2020-05-27 21:39:57 -0500

    Finished exercise2

--- exercise2.js
+++ exercise2.js
@@ -57,10 +57,12 @@ console.log(`20 is ${!!20 == true} because positive nonzero numbers are truthy`)
 console.log(`0 is ${!!0 == true} because zeros are falsy`);
 
 // "zero";
-console.log(`"zero" is ${!!"zero" == true} because strings are truthy`);
+console.log(`"zero" is ${!!("zero") == true} because strings are truthy`);
 
 // const zero = 20;
 
+const zero = 20
+console.log(`const zero = 20; is ${!!zero} because defined variables are truthy`);
 
 // null
 console.log(`null is ${!!null == true} because null is always falsy`);
@@ -69,10 +71,10 @@ console.log(`null is ${!!null == true} because null is always falsy`);
 console.log(`"0" is ${!!"0" == true} because strings are truthy`);
 
 // !""
-
+console.log(`!"" is ${!("")} because the opposite of the boolean value of an empty string is truthy`);
 
 // {}
-
+console.log(`{} is ${!!{} == true} because objects are truthy`);
 
 // () => {console.log("hello TEKcamp!");
 console.log(`() => {console.log("hello TEKcamp!"); is ${!!(() => {console.log("hello TEKcamp!");})} because functions are truthy`);
@@ -213,7 +215,13 @@ let animal = {
 //Problem 7: create a function that outputs your 3 favorite data types, with a message explaining why you like that data type.
 
 //your code...
-
+let favTypes = function() {
+    console.log("String - because you can write beautiful poetry with them");
+    console.log("Array - because I can store my grocery list in them");
+    console.log("Object - because they can describe the traits of my favorite things");
+  }
+  
+  favTypes();
 
 
 /************************************************************* */
@@ -222,6 +230,17 @@ let animal = {
 //1. Create a Higher Order Function called multiple(x) that takes a single parameter.  This HOF should return another function fn(y) that takes another single parameter y.  This inner function should compute the product of it's parameter with the parameter passed into multiple.  Use this returned "first-class" function to compute triples of any given number.
 
 //your code...
+let multiple = function(x) {
+    let fn = function(y) {
+        return x * y;
+    }
+    return fn;
+}
+
+let func = multiple(3);
+
+console.log(func(func(3)));
+
 
 
 // 2. Write an outer function called stockGain that has cost basis (basis) as a parameter; declare a variable called message that holds " is how much the stock has increased".  Return an inner function with years (yrs) as a parameter and declare a variable for growth rate (r) of 5%. Console log your calculation.
@@ -229,5 +248,16 @@ let animal = {
 // Once finished, declare a variable called futureValue that holds your stockGain function and enter any amount for the cost basis and a number for the number of years.  Run the function returned by the higher order function to display the future value of the stock.  
 
 //your code...
+let stockGain = function(basis) {
+    let message = " is how much the stock has increased";
+    let years = function(yrs) {
+        let r = 0.05
+        console.log(((basis * yrs) * r) + message);
+    }
+    return years;
+}
 
+stockGain(100)(1);
 
+let futureValue = stockGain(100);
+futureValue(5);
